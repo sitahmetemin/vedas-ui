bbd1094e05d2878e35ce1b2f3079b476fd32df6c
Ahmet Emin ŞİT <sitahmetemin@hotmail.com>
2019-05-06 16:34:42 +0300

    dropdown table hatası giderildi

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,6 +1,23 @@
+//Menü açılıp kapanma olayı burada yapıldı
+$('#sidebarClose').on('click', function () {
+
+    let status = $('#content').attr('class');
+
+    if (status.includes('content-fit')) {
+
+        $('#content').attr('class', 'col-md-12 ');
+        $('#sidebar').css('left', '-500px');
+        $('#sidebarIcon').attr('class', 'fas fa-bars');
+    } else {
+        $('#content').attr('class', 'col-md-12 content-fit');
+        $('#sidebar').css('left', '-10px');
+        $('#sidebarIcon').attr('class', 'fa fa-times');
+    }
+});
+
 $(document).ready(function () {
-    $('.selet2').select2({
-        allowClear: true,
-        language: "tr"
-    });
+
+    console.log("Vedaş Projesine Hoş Geldiniz.")
+
+
 });
